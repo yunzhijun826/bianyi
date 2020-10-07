8a7414de9f6a11182d6dae1f53936b2fe8fda678
yunzhijun826 <63158918+yunzhijun826@users.noreply.github.com>
2020-10-07 13:32:03 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# -- Dockerfile --
+# 这个文件负责构建包含你的程序的 Docker 容器
+
+# 使用 Java 13
+FROM openjdk:13-alpine
+# 向容器内复制文件
+COPY ./* /app/
+# 编译程序
+WORKDIR /app/
+RUN javac -d ./output ./Main.java
+# 将当前目录设为输出目录
+WORKDIR /app/output
---
+++ Main.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.String;
+public class Main {
+    public ArrayList<String> token = new ArrayList<String>();
+    public void addList(){
+        token.add("BEGIN");
+        token.add("END");
+        token.add("FOR");
+        token.add("IF");
+        token.add("THEN");
+        token.add("ELSE");
+    }
+    public void readFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        Reader reader = null;
+        reader = new InputStreamReader(new FileInputStream(file));
+        int tempchar;
+        while ((tempchar = reader.read()) != -1) {
+            while((char)tempchar == '\n'||(char)tempchar == ' '||(char)tempchar == '\r'){
+                tempchar = reader.read();
+            }
+            if((char)tempchar>='a'&&(char)tempchar<='z'||(char)tempchar>='A'&&(char)tempchar<='Z'){
+                String s = "";
+                //System.out.println(s);
+//                s.append((char)tempchar);
+                s=s+(char)tempchar;
+                tempchar = reader.read();
+                while((char)tempchar>='a'&&(char)tempchar<='z'||(char)tempchar>='A'&&(char)tempchar<='Z'||(char)tempchar>='0'&&(char)tempchar<='9'){
+//                    s.append((char)tempchar);
+                    s=s+(char)tempchar;
+                    tempchar = reader.read();
+                }
+                if(token.contains(s)){
+//                    s.substring(0, 1).toUpperCase();
+//                    s.substring(1).toLowerCase();
+                    System.out.println(s.substring(0, 1).toUpperCase()+s.substring(1).toLowerCase());
+                }
+                else{
+                    System.out.println("Ident("+s+")");
+                }
+            }
+            else if(Character.isDigit((char)tempchar)){
+                String Int="";
+                Int=Int+(char)tempchar;
+                tempchar = reader.read();
+                while((char)tempchar>='0'&&(char)tempchar<='9'){
+                    Int=Int+(char)tempchar;
+                    tempchar = reader.read();
+                }
+                while(Int.charAt(0)=='0'&&Int.length()!=1){
+                    Int=Int.substring(1);
+                }
+                System.out.println("Int("+Int+")");
+                if((char)tempchar>='a'&&(char)tempchar<='z'||(char)tempchar>='A'&&(char)tempchar<='Z'){
+                    String s = "";
+                    s=s+(char)tempchar;
+                    tempchar = reader.read();
+                    while((char)tempchar>='a'&&(char)tempchar<='z'||(char)tempchar>='A'&&(char)tempchar<='Z'||(char)tempchar>='0'&&(char)tempchar<='9'){
+//                    s.append((char)tempchar);
+                        s=s+(char)tempchar;
+                        tempchar = reader.read();
+                    }
+                    if(token.contains(s)){
+                        System.out.println(s.substring(0, 1).toUpperCase()+s.substring(1).toLowerCase());
+                    }
+                    else{
+                        System.out.println("Ident("+s+")");
+                    }
+                }
+            }
+            else if((char)tempchar=='+'){
+                System.out.println("Plus");
+            }
+            else if((char)tempchar==','){
+                System.out.println("Comma");
+            }
+            else if((char)tempchar=='*'){
+                System.out.println("Star");
+            }
+            else if((char)tempchar=='('){
+                System.out.println("LParenthesis");
+            }
+            else if((char)tempchar==')'){
+                System.out.println("RParenthesis");
+            }
+            else if((char)tempchar==':'){
+                tempchar = reader.read();
+                if((char)tempchar=='='){
+                    System.out.println("Assign");
+                }
+                else{
+                    System.out.println("Colon");
+                }
+            }
+            else{
+                System.out.println("Unknown");
+                break;
+            }
+        }
+        reader.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m=new Main();
+        m.addList();
+        //Scanner input=new Scanner(System.in);
+        String filePath=args[0];
+        //String filePath=input.next();
+        m.readFile(filePath);
+
+    }
+}
